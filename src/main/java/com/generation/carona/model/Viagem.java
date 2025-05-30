cc6aa48ed0b5aa087e9e0f924fe0fe573e072a8e
Victor Hugo <projektvek@gmail.com>
2025-05-30 10:09:53 -0300

    :sparkles: Criada entidade Viagem

---
+++ src/main/java/com/generation/carona/model/Viagem.java
@@ -0,0 +1,47 @@
+package com.generation.carona.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_viagens")
+public class Viagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O Atributo idMotorista é Obrigatório!")
+	private Long idMotorista;
+	
+	@NotNull(message = "O Atributo idPassageiro é Obrigatório!")
+	private Long idPassageiro;
+	
+	@NotNull(message = "O Atributo idVeiculo é Obrigatório!")
+	private Long idVeiculo;
+	
+	@Column(length = 1000)
+	@Size(min = 10, max = 1000, message = "O atributo partida deve ter no mínimo 10 e no máximo 1000 caracteres")
+	private String partida;
+	
+	@Column(length = 1000)
+	@Size(min = 10, max = 1000, message = "O atributo destino deve ter no mínimo 10 e no máximo 1000 caracteres")
+	private String destino;
+	
+	@Positive
+	@Column(precision = 7, scale = 2)
+	private BigDecimal distancia;
+	
+	@Positive
+	@Column(precision = 5, scale = 2)
+	private BigDecimal tempoDeViagem;
+}
