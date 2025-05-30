0bae76bc59b1f997a99480c13be5c5aca01412a7
Eduardo Tosta <eduardo_tosta@outlook.com>
2025-05-30 10:59:34 -0300

    Aplication properties corrigido

--- src/main/java/com/generation/carona/controller/ViagemController.java
+++ src/main/java/com/generation/carona/controller/ViagemController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.carona.model.Viagem;
 import com.generation.carona.repository.ViagemRepository;
 
 import jakarta.validation.Valid;
